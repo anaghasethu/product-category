87f66705aea63ac5cbd7674678f7e186a1b1fccd
anaghasethu <anaghasethu1998@gmail.com>
2022-08-02 02:10:36 +0530

    first commit

---
+++ manageproduct/src/main/java/com/example/demo/entity/ProductCategory.java
@@ -0,0 +1,44 @@
+package com.example.demo.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "productcategory")
+public class ProductCategory {
+	
+	@Id
+	@Column(name = "categoryid")
+	private int categoryid;
+	@Column(name = "categoryname")
+	private String categoryname;
+	public ProductCategory() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public ProductCategory(int categoryid, String categoryname) {
+		super();
+		this.categoryid = categoryid;
+		this.categoryname = categoryname;
+	}
+	public int getCategoryid() {
+		return categoryid;
+	}
+	public void setCategoryid(int categoryid) {
+		this.categoryid = categoryid;
+	}
+	public String getCategoryname() {
+		return categoryname;
+	}
+	public void setCategoryname(String categoryname) {
+		this.categoryname = categoryname;
+	}
+	@Override
+	public String toString() {
+		return "ProductCategory [categoryid=" + categoryid + ", categoryname=" + categoryname + "]";
+	}
+
+	
+}
